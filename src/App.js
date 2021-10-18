fc51765a934eb6d276189a0d485d6ba91968ac79
JEHYEOK <seaving@naver.com>
2021-10-18 23:11:37 +0900

    map.props test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+function Food({ favorite, sort }) {
+  return (
+    <div>
+      <h1>
+        I love {sort} .st {favorite} food{" "}
+      </h1>
+    </div>
+  );
+}
 
+const foodILike = [
+  {
+    name: "kimchi",
+    sort: 10,
+  },
+  {
+    name: "orange",
+    sort: 20,
+  },
+  {
+    name: "chicken",
+    sort: 30,
+  },
+  {
+    name: "ggukumi",
+    sort: 40,
+  },
+];
+function renderFood(dish){
+  console.log(dish);
+  return <Food favorite={dish.name} sort={dish.sort}/>
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(renderFood)};
     </div>
+    
   );
 }
 
